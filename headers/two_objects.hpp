4eefacd808e97be3e6f6a0d985f304de34ce3461
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2023-12-29 13:58:08 +0300

    Add implementation of getObjSplittedByOneWay function for basic data types

--- headers/two_objects.hpp
+++ headers/two_objects.hpp
@@ -1,5 +1,11 @@
 #ifndef TWO_OBJECTS_HPP
 #define TWO_OBJECTS_HPP
 
+#include "func_ptr_operations.hpp"
+
+template <typename T1, typename T2, typename Func>
+auto getObjSplittedByOneWay(T1 first, T2 second, Func way) {
+    return (*way)(first, second);
+}
 
 #endif // !TWO_OBJECTS_HPP
