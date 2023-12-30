9aed841b40faf2ef651e62767b60f94dd1533cd6
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2023-12-30 14:52:27 +0300

    Add implementation for findFirstIf function for basic arrays

---
+++ headers/One_object/find_first_if.hpp
@@ -0,0 +1,9 @@
+#ifndef FIND_FIRST_IF_HPP
+#define FIND_FIRST_IF_HPP
+
+template <typename T, std::size_t COUNT, typename PredT>
+const T* findFirstIf(const T (&arr)[COUNT], PredT predicate);
+
+#include"find_first_if_impl.ipp"
+
+#endif // !FIND_FIRST_IF_HPP
