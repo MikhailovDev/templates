fb0da6e075e2012430b9143a3756b383f13a48ef
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2023-12-30 14:52:27 +0300

    Add implementation for makeOperationOnEachElem function for basic types

--- headers/One_object/make_operation_on_each_elem.hpp
+++ headers/One_object/make_operation_on_each_elem.hpp
@@ -1,6 +1,9 @@
 #ifndef MAKE_OPERATION_ON_EACH_ELEM_HPP
 #define MAKE_OPERATION_ON_EACH_ELEM_HPP
 
+template <typename T, typename OperationT>
+void makeOperationOnEachElem(T& elem, OperationT operation);
+
 #include "make_operation_on_each_elem_impl.ipp";
 
 #endif // !MAKE_OPERATION_ON_EACH_ELEM_HPP
--- headers/One_object/make_operation_on_each_elem_impl.ipp
+++ headers/One_object/make_operation_on_each_elem_impl.ipp
@@ -3,4 +3,9 @@
 
 #include "make_operation_on_each_elem.hpp"
 
+template <typename T, typename OperationT>
+void makeOperationOnEachElem(T& elem, OperationT operation) {
+    elem = (*operation)(elem);
+}
+
 #endif // !MAKE_OPERATION_ON_EACH_ELEM_IMPL_IPP
